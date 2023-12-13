a019f155b3d3ea45a543181657dea63a29e8b5b9
Akshat Gupta <akshatgtc@gmail.com>
2023-12-13 20:58:47 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sessionStore = mongoStore.create({
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 
 app.use(
   session({
