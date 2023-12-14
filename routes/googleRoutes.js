5eb66902a48541fd0c55624c95c31f55b713df34
Akshat Gupta <akshatgtc@gmail.com>
2023-12-14 18:52:29 +0530

    latest

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -45,8 +45,7 @@ router.get("/success", async (req, res) => {
     res.cookie('token', token, {
         maxAge: 24 * 60 * 60 * 1000, 
         httpOnly: false, 
-        secure: true,
-        sameSite: 'strict' 
+        secure: false,
       });
     return res.redirect("http://localhost:3000/aqify#/MainDashboard/Dashbaord");
 });
