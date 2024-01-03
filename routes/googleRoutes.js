73439c4d366c116aebc17553a9e61a7e4bf0a31e
Eeshaan Sethia <eeshaans05@gmail.com>
2024-01-04 00:01:29 +0530

    Updated link error

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -47,6 +47,6 @@ router.get("/success", async (req, res) => {
         httpOnly: false,
         secure: false,
     });
-    return res.redirect(process.env.FRONTEND_URL + `/aqify#/MainDashboard/Dashbaord`);
+    return res.redirect(process.env.FRONTEND_URL + `/#/MainDashboard/Dashbaord`);
 });
 module.exports = router;
